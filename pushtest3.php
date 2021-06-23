c69b71a7250c58a0e8d0605e02d7988d7d27a2c7
raintzg <rita.0139@gmail.com>
2021-06-24 01:19:08 +0900

    pushの確認commit1

---
+++ pushtest3.php
@@ -0,0 +1,16 @@
+<?php
+$animals = array('Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Sheep', 'Monkey', 'Rooster', 'Dog', 'Boar', 'Cat');
+foreach ($animals as $value) {
+	echo $value;
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>課題20</title>
+</head>
+<body>
+<h1>十二支</h1>
+</body>
+</html>
