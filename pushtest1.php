8f4606f954aaf6ec3820230e3fe2338efbf0630a
raintzg <rita.0139@gmail.com>
2021-06-23 23:49:33 +0900

    pushの確認1

---
+++ pushtest1.php
@@ -0,0 +1,13 @@
+<?php
+$animals = array('Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon');
+?>
+<!DOCTYPE hmtl>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>課題20</title>
+</head>
+<body>
+<?php echo $animals[3]; ?>
+</body>
+</html>
